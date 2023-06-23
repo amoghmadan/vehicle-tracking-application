e23975095a727e5b0762a87829e36b46a179f9f6
Gaurav Singh <gauravsinghgzp83@gmail.com>
2023-06-23 16:00:31 +0530

    Change the URL of Mongodb

--- vehicle_tracker_api/src/settings.js
+++ vehicle_tracker_api/src/settings.js
@@ -2,6 +2,6 @@ import path from 'path';
 
 export const BASE_DIR = path.dirname(path.dirname(__filename));
 
-export const MONGO_URI = 'mongo://localhost:27017/vehicletracker';
+export const MONGO_URI = 'mongodb://127.0.0.1:27017/vehicletracker';
 
 export const SALT = 12;

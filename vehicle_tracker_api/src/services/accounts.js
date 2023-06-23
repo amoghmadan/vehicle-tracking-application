1905497ccf1139d2d19bbe655e54fda4232f4ba9
Gaurav Singh <gauravsinghgzp83@gmail.com>
2023-06-23 16:03:28 +0530

    Service function for login, details and logout api

--- vehicle_tracker_api/src/services/accounts.js
+++ vehicle_tracker_api/src/services/accounts.js
@@ -1,5 +1,35 @@
-export async function performAccountLogin(payload) { }
+import { compareSync } from 'bcryptjs';
+import { User } from '../models';
+import { generateKey } from '../utils/token';
 
-export async function retrieveAccountDetail() { }
+export async function performLoginService(payload) {
+  const user = await User.findOne({ email: payload.email, isActive: true });
 
-export async function performAccountLogout() { }
+  if (!user) return null;
+  if (!compareSync(payload.password, user.password)) return null;
+  if (!user.token) {
+    user.token = { key: generateKey() };
+    user.lastLogin = user.token.created;
+    await user.save();
+  }
+
+  return { token: user.token.key };
+}
+
+export async function retrieveUserService(user) {
+  const detail = {
+    email: user.email,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    dateJoined: user.dateJoined,
+  };
+
+  return detail;
+}
+
+export async function performLogoutService(user) {
+  user.set('token', undefined, { strict: false });
+  await user.save();
+
+  return {};
+}

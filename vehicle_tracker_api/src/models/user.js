ccfea0a87fe6bf85663ba38608590f2adfbb17a1
Amogh Madan <amoghmadaan@gmail.com>
2023-07-07 19:49:44 +0530

    Add pre save and validatePassword to generate and match password hash.

--- vehicle_tracker_api/src/models/user.js
+++ vehicle_tracker_api/src/models/user.js
@@ -16,6 +16,27 @@ const userSchema = new Schema({
   token: {type: tokenSchema, require: false},
 });
 
+userSchema.pre('save', async function(next) {
+  // eslint-disable-next-line no-invalid-this
+  if (!this.isModified('password')) return next();
+  try {
+    const salt = await genSalt(12);
+    // eslint-disable-next-line no-invalid-this
+    const hashedPassword = await hash(this.password, salt);
+    // eslint-disable-next-line no-invalid-this
+    this.password = hashedPassword;
+    return next();
+  } catch (e) {
+    return next(e);
+  }
+});
+
+userSchema.methods.validatePassword = async function(candidatePassword) {
+  // eslint-disable-next-line no-invalid-this
+  const success = await compare(candidatePassword, this.password);
+  return success;
+};
+
 const User = model('User', userSchema);
 
 export default User;

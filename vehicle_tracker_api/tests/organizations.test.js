06a85ecf707086b01b5d610f8f8c39ddc380769c
Gaurav Singh <gauravsinghgzp83@gmail.com>
2023-08-24 14:55:36 +0530

    Create test cases for tracker and organization

--- vehicle_tracker_api/tests/organizations.test.js
+++ vehicle_tracker_api/tests/organizations.test.js
@@ -1,38 +1,163 @@
-// import 'dotenv/config';
-// import mongoose from 'mongoose';
-// import request from 'supertest';
+import 'dotenv/config';
+import mongoose from 'mongoose';
+import request from 'supertest';
 
-// import {getRequestListener} from '../src/cli/bootstrap';
-// import {User} from '../src/models';
-// import {generateKey} from '../src/utils/token';
+import {getRequestListener} from '../src/cli/bootstrap';
+import {Organization} from '../src/models';
 
-// const app = getRequestListener();
+const app = getRequestListener();
 
-// let user;
-// let token;
+describe('Organization API Tests', () => {
+  let authToken;
 
-// beforeEach(async () => {
-//   await mongoose.connect(process.env.MONGO_URI);
-//   user = await User.findOne({email: 'gauravsinghgzp83@gmail.com'});
-//   if (!user.token) {
-//     user.token = {key: generateKey()};
-//     user.lastLogin = new Date();
-//     await user.save();
-//   }
-//   token = user.token.key;
-// });
+  beforeAll(async () => {
+    await mongoose.connect(process.env.MONGO_URI);
 
-// afterEach(async () => {
-//   user.set('token', undefined, {strict: false});
-//   await user.save();
-//   await mongoose.connection.close();
-// });
+    const loginPayload = {
+      email: 'test.user@email.com',
+      password: 'foo',
+    };
 
-// describe('GET /api/v1/organizations/', () => {
-//   it('List All Organizations', async () => {
-//     const response = await request(app)
-//         .get('/api/v1/organizations/')
-//         .set({Authorization: `Token ${token}`});
-//     expect(response.status).toBe(200);
-//   });
-// });
+    const loginResponse = await request(app)
+        .post('/api/v1/accounts/login')
+        .send(loginPayload);
+
+    authToken = loginResponse.body.token;
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+
+  beforeEach(async () => {
+    await Organization.deleteMany({});
+  });
+
+  afterEach(async () => {
+    await Organization.deleteMany({});
+  });
+
+  describe('POST /api/v1/organizations', () => {
+    it('Creates a new Organization', async () => {
+      const payload = {
+        name: 'Test Organization',
+        description: 'Test description',
+      };
+
+      const response = await request(app)
+          .post('/api/v1/organizations')
+          .set('Authorization', `Token ${authToken}`)
+          .send(payload);
+
+      expect(response.status).toBe(201);
+      expect(response.body.name).toBe(payload.name);
+      expect(response.body.description).toBe(payload.description);
+    });
+  });
+
+  describe('GET /api/v1/organizations', () => {
+    it('Retrieves a list of Organizations', async () => {
+      await Organization.create({
+        name: 'Organization 1',
+        description: 'Description 1',
+      });
+      await Organization.create({
+        name: 'Organization 2',
+        description: 'Description 2',
+      });
+
+      const response = await request(app)
+          .get('/api/v1/organizations')
+          .set('Authorization', `Token ${authToken}`);
+
+      expect(response.status).toBe(200);
+      expect(Array.isArray(response.body)).toBe(true);
+      expect(response.body.length).toBe(2);
+    });
+  });
+
+  describe('GET /api/v1/organizations/:id', () => {
+    it('Retrieves an Organization by ID', async () => {
+      const organization = await Organization.create({
+        name: 'Test Organization',
+        description: 'Test description',
+      });
+
+      const response = await request(app)
+          .get(`/api/v1/organizations/${organization._id}`)
+          .set('Authorization', `Token ${authToken}`);
+
+      expect(response.status).toBe(200);
+      expect(response.body._id).toBe(organization._id.toString());
+      expect(response.body.name).toBe(organization.name);
+      expect(response.body.description).toBe(organization.description);
+    });
+
+    it('Returns 404 if Organization not found', async () => {
+      const response = await request(app)
+          .get('/api/v1/organizations/123456789')
+          .set('Authorization', `Token ${authToken}`);
+
+      expect(response.status).toBe(404);
+    });
+  });
+
+  describe('PATCH /api/v1/organizations/:id', () => {
+    it('Updates an Organization partially', async () => {
+      const organization = await Organization.create({
+        name: 'Test Organization',
+        description: 'Test description',
+      });
+
+      const payload = {
+        name: 'Updated Organization',
+      };
+
+      const response = await request(app)
+          .patch(`/api/v1/organizations/${organization._id}`)
+          .set('Authorization', `Token ${authToken}`)
+          .send(payload);
+
+      expect(response.status).toBe(200);
+      expect(response.body._id).toBe(organization._id.toString());
+      expect(response.body.name).toBe(payload.name);
+      expect(response.body.description).toBe(organization.description);
+    });
+
+    it('Returns 404 if Organization not found', async () => {
+      const payload = {
+        name: 'Updated Organization',
+      };
+
+      const response = await request(app)
+          .patch('/api/v1/organizations/123456789')
+          .set('Authorization', `Token ${authToken}`)
+          .send(payload);
+
+      expect(response.status).toBe(404);
+    });
+  });
+
+  describe('DELETE /api/v1/organizations/:id', () => {
+    it('Deletes an Organization', async () => {
+      const organization = await Organization.create({
+        name: 'Test Organization',
+        description: 'Test description',
+      });
+
+      const response = await request(app)
+          .delete(`/api/v1/organizations/${organization._id}`)
+          .set('Authorization', `Token ${authToken}`);
+
+      expect(response.status).toBe(204);
+    });
+
+    it('Returns 404 if Organization not found', async () => {
+      const response = await request(app)
+          .delete('/api/v1/organizations/123456789')
+          .set('Authorization', `Token ${authToken}`);
+
+      expect(response.status).toBe(404);
+    });
+  });
+});
--- vehicle_tracker_api/tests/trackers.test.js
+++ vehicle_tracker_api/tests/trackers.test.js
@@ -1,29 +1,112 @@
-// import 'dotenv/config';
-// import mongoose from 'mongoose';
-// import request from 'supertest';
+import mongoose from 'mongoose';
+import {Tracker} from '../models';
 
-// import {getRequestListener} from '../src/cli/bootstrap';
-// import {User} from '../src/models';
-// import {generateKey} from '../src/utils/token';
+describe('Tracker Service Tests', () => {
+  beforeAll(async () => {
+    await mongoose.connect(process.env.MONGO_URI);
+  });
 
-// const app = getRequestListener();
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
 
-// let user;
-// let token;
+  beforeEach(async () => {
+    await Tracker.deleteMany({});
+  });
 
-// beforeEach(async () => {
-//   await mongoose.connect(process.env.MONGO_URI);
-//   user = await User.findOne({email: 'gauravsinghgzp83@gmail.com'});
-//   if (!user.token) {
-//     user.token = {key: generateKey()};
-//     user.lastLogin = new Date();
-//     await user.save();
-//   }
-//   token = user.token.key;
-// });
+  afterEach(async () => {
+    await Tracker.deleteMany({});
+  });
 
-// afterEach(async () => {
-//   user.set('token', undefined, {strict: false});
-//   await user.save();
-//   await mongoose.connection.close();
-// });
+  describe('retrieveTrackerService', () => {
+    it('Retrieves a tracker by ID', async () => {
+      const trackerData = { /* mock tracker data */ };
+      Tracker.findById.mockResolvedValue(trackerData);
+
+      const result = await retrieveTrackerService('trackerId');
+      expect(result).toEqual(trackerData);
+    });
+
+    it('Returns null if tracker is not found', async () => {
+      Tracker.findById.mockResolvedValue(null);
+
+      const result = await retrieveTrackerService('nonExistentTrackerId');
+      expect(result).toBeNull();
+    });
+  });
+
+  describe('createTrackerService', () => {
+    it('Creates a new tracker', async () => {
+      const payload = { /* mock payload */ };
+      const user = {_id: 'userId'};
+      const createdTracker = { /* mock created tracker */ };
+      Tracker.prototype.save = jest.fn().mockResolvedValue(createdTracker);
+
+      const result = await createTrackerService(payload, user);
+      expect(result).toEqual(createdTracker);
+    });
+  });
+
+  describe('updateTrackerService', () => {
+    it('Updates a tracker by ID', async () => {
+      const updatedTracker = { /* mock updated tracker */ };
+      Tracker.findByIdAndUpdate.mockResolvedValue(updatedTracker);
+
+      const result = await updateTrackerService('trackerId', {});
+      expect(result).toEqual(updatedTracker);
+    });
+
+    it('Returns null if tracker is not found', async () => {
+      Tracker.findByIdAndUpdate.mockResolvedValue(null);
+
+      const result = await updateTrackerService('nonExistentTrackerId', {});
+      expect(result).toBeNull();
+    });
+  });
+
+  describe('listTrackerService', () => {
+    it('Retrieves a list of trackers for a date', async () => {
+      const query = { /* mock query */ };
+      const trackers = [{ /* mock tracker 1 */ }, { /* mock tracker 2 */ }];
+      Tracker.find.mockResolvedValue(trackers);
+
+      const result = await listTrackerService(query);
+      expect(result).toEqual(trackers);
+    });
+
+    it('Retrieves a list of trackers for current date if no date is provided',
+        async () => {
+          const currentDate = new Date().toISOString().split('T')[0];
+          const query = {};
+          const trackers = [{ /* mock tracker 1 */ }, { /* mock tracker 2 */ }];
+          Tracker.find.mockResolvedValue(trackers);
+
+          const result = await listTrackerService(query);
+          expect(result).toEqual(trackers);
+          expect(Tracker.find).toHaveBeenCalledWith({
+            created: {
+              $gte: new Date(`${currentDate}T00:00:00Z`),
+              $lte: new Date(`${currentDate}T23:59:59Z`),
+            },
+          });
+        });
+  });
+
+  describe('leftFacilityService', () => {
+    it('Marks out time for a tracker', async () => {
+      const user = {_id: 'userId'};
+      const updatedTracker = { /* mock updated tracker */ };
+      Tracker.findByIdAndUpdate.mockResolvedValue(updatedTracker);
+
+      const result = await leftFacilityService('trackerId', user);
+      expect(result).toEqual(updatedTracker);
+    });
+
+    it('Returns null if tracker is not found', async () => {
+      Tracker.findByIdAndUpdate.mockResolvedValue(null);
+
+      const result = await leftFacilityService('nonExistentTrackerId', {});
+      expect(result).toBeNull();
+    });
+  });
+});
